1ae24c54c0b6d2356b7fa8658f4220bddcb66749
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-05-21 14:51:02 +0100

    working

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
- class Series {
-    constructor(input) {
-      this.input = input
-      this.inputArray = this.input.split('').map(a => Number(a))
-    }
-  //type code here
-  
+class Series {
+  constructor(str) {
+    this.digits = str.match(/\d/g).map(d => Number.parseInt(d, 10));
   }
 
+  slices(n) {
+    if (n > this.digits.length) throw new Error('Slice size is too big.');
+    const result = [];
+    for (let i = 0; i <= this.digits.length - n; i++) {
+      result.push(this.digits.slice(i, i + n));
+    }
+    return result;
+  }
+}
   module.exports = Series;
